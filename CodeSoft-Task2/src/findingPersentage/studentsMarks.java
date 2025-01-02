fffe4ba42fcd421b6ef5266486a62a77b027dce2
NASHEED SHAIK <NASHEED SHAIK@MEERA>
2025-01-02 16:18:45 +0530

    added sccanner to read

--- CodeSoft-Task2/src/findingPersentage/studentsMarks.java
+++ CodeSoft-Task2/src/findingPersentage/studentsMarks.java
@@ -1,33 +1,34 @@
 package findingPersentage;
 
+import java.util.Scanner;
+
 public class studentsMarks {
-	public static double calPersentage(int[] marks,int totalmarks) {
-		int sum=0; 
-		for(int mark:marks) {
-			sum+=mark;
-		}
-		double persentage=((double)sum/totalmarks)*100;
-				return persentage;
-	}
-	public static void main(String[] args) {
-		int[] marks= {90,90,90,90,90,90};
-		int totalmarks=100*marks.length;
-		double persentage=calPersentage(marks, totalmarks);
-		System.out.println(persentage);
-	}
 	
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * //public calPersentage(int[] marks,int totalmarks) {
-	 * 
-	 * int sum=0;
-	 * 
-	 * int[] marks= {90,90,90,90,90,90}; int totalmarks=100*marks.length;
-	 * 
-	 * for(int mark:marks) { sum+=mark; } double
-	 * persentage=((double)sum/totalmarks)*100;
-	 * 
-	 * System.out.println(persentage); }
-	 */
-}
+	    public static void main(String[] args) {
+	    	
+	        Scanner s= new Scanner(System.in);
+
+	        System.out.print("Enter the total number of subjects: ");
+	        int noOfSubj = s.nextInt();
+
+	        System.out.print("Enter the max marks");
+	        double maxMarks = s.nextDouble();
+
+	        double[] marks = new double[noOfSubj];
+	        double totalMarks = 0;
+
+	        for (int i = 0; i < noOfSubj; i++) {
+	            System.out.print("Enter marks of subject " + (i + 1) + ": ");
+	            marks[i] = s.nextDouble();
+	            totalMarks += marks[i]; 
+	        }
+	        double averageMarks = totalMarks / noOfSubj;
+	        double percentage=(totalMarks/maxMarks)*100;
+
+	        System.out.println("Total Marks: " + totalMarks);
+	        System.out.println("Average Marks: " + averageMarks);
+	        System.out.println("Percentage: " + percentage + "%");
+
+	        s.close();
+	    }
+	}
